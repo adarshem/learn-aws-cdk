1194a942081c0a29d0c7acc9846a5444fe4755ed
adarshem <adarshem@gmail.com>
2025-04-18 14:05:51 -0700

    Added unit test

--- test/hello-cdk.test.ts
+++ test/hello-cdk.test.ts
@@ -1,17 +1,17 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as HelloCdk from '../lib/hello-cdk-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as HelloCdk from '../lib/hello-cdk-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/hello-cdk-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new HelloCdk.HelloCdkStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('Lambda function created', () => {
+  const app = new cdk.App();
+  const stack = new HelloCdk.HelloCdkStack(app, 'MyTestStack');
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  template.hasResourceProperties('AWS::Lambda::Function', {
+    Handler: 'index.handler',
+    Runtime: 'nodejs20.x'
+  });
+  template.hasResourceProperties('AWS::Lambda::Url', {
+    AuthType: 'NONE'
+  });
 });
