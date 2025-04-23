6018b316d2bb1f6f7c329035509e74fc86bce83b
adarshem <adarshem@gmail.com>
2025-04-22 20:27:18 -0700

    Updated app to use EventBridge stack

--- bin/hello-cdk.ts
+++ bin/hello-cdk.ts
@@ -2,30 +2,38 @@
 import * as cdk from 'aws-cdk-lib';
 import { HelloCdkStack } from '../lib/hello-cdk-stack';
 import { MiniCdkDemoStack } from '../lib/mini-cdk-demp-stack';
+import { EventBridgeStack } from '../lib/event-bridge-stack';
 
 const app = new cdk.App();
 
-new HelloCdkStack(app, 'HelloCdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+// new HelloCdkStack(app, 'HelloCdkStack', {
+//   /* If you don't specify 'env', this stack will be environment-agnostic.
+//    * Account/Region-dependent features and context lookups will not work,
+//    * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION
-  }
+//   /* Uncomment the next line to specialize this stack for the AWS Account
+//    * and Region that are implied by the current CLI configuration. */
+//   env: {
+//     account: process.env.CDK_DEFAULT_ACCOUNT,
+//     region: process.env.CDK_DEFAULT_REGION
+//   }
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+//   /* Uncomment the next line if you know exactly what Account and Region you
+//    * want to deploy the stack to. */
+//   // env: { account: '123456789012', region: 'us-east-1' },
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+//   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+// });
 
-// Stack containing SQS and Lambda
-new MiniCdkDemoStack(app, 'MiniCdkDemoStack', {
+// // Stack containing SQS and Lambda
+// new MiniCdkDemoStack(app, 'MiniCdkDemoStack', {
+//   env: {
+//     account: process.env.CDK_DEFAULT_ACCOUNT,
+//     region: process.env.CDK_DEFAULT_REGION
+//   }
+// });
+
+new EventBridgeStack(app, 'EventBridgeStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION
